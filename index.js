e7a068adfe520b99d4631d1c2690b5bf0d987159
Napatsakorn Pianchana <napatdmsu@gmail.com>
2022-07-07 14:58:06 +0700

    createGenerateFieldMethod

--- index.js
+++ index.js
@@ -1,147 +1,165 @@
-const prompt = require('prompt-sync')({ sigint: true });
+// const prompt = require('prompt-sync')({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+// const hat = '^';
+// const hole = 'O';
+// const fieldCharacter = '░';
+// const pathCharacter = '*';
+// const fieldRandom = ['░', 'O', '^']
+// class Field {
+//     constructor() {
+//         this.field = [
+//             ['*', '░', 'O'],
+//             ['░', 'O', '░'],
+//             ['░', '^', '░'],]
 
-class Field {
-    constructor() {
-        this.board = [
-            ['*', '░', 'O'],
-            ['░', 'O', '░'],
-            ['░', '^', '░'],]
+//         this.playerPosition = [0, 0];
+//         // this.inputField = [[inputField]];
+//     }
 
-        this.playerPosition = [0, 0];
-    }
+// // เอาไปใส่ class ยังไงวะะะะะะะะะะะะะะะะะะะ
+//     // generateField(height, width) {
+//     //     const newField = []
+//     //     const fieldRandom = ['░', 'O', '^']
+//     //     newField[0][1] = hat;
+//     //     for (let i = 0; i < height; i++) {
+//     //         newField.push([])
+//     //         for (let j = 0; j < width; j++) {
+//     //             var item = fieldRandom[Math.floor(Math.random()*fieldRandom.length)];
+//     //             newField[i].push(item)
+//     //         }
+//     //     } return newField;
+//     // }
+   
+//     mvRight() {
+//         if (this.playerPosition[0] + 1 < this.field[0].length) {
+//             this.updateField(this.playerPosition, '*');
+//             this.playerPosition[0]++;
+//             this.updateField(this.playerPosition, '*');
+//         } else {
+//             console.log('You lose!!! move outside the field.')
+//             throw Error()
+//         }
+//     }
 
-    mvRight() {
-        if (this.playerPosition[0] + 1 < this.board[0].length) {
-            this.updateField(this.playerPosition, '*');
-            this.playerPosition[0]++;
-            this.updateField(this.playerPosition, '*');
-        } else {
-            console.log('You lose!!! You fell down a hole.')
-            throw Error()
-        }
-    }
+//     mvLeft() {
+//         if (this.playerPosition[0] - 1 >= 0) {
+//             this.updateField(this.playerPosition, '*');
+//             this.playerPosition[0]--;
+//             this.updateField(this.playerPosition, '*');
+//         } else {
+//             console.log('You lose!!! move outside the field.')
+//             throw Error()
+//         }
+//     }
 
-    mvLeft() {
-        if (this.playerPosition[0] - 1 >= 0) {
-            this.updateField(this.playerPosition, '*');
-            this.playerPosition[0]--;
-            this.updateField(this.playerPosition, '*');
-        } else {
-            console.log('You lose!!! You fell down a hole.')
-            throw Error()
-        }
-    }
+//     mvUp() {
+//         if (this.playerPosition[1] - 1 >= 0) {
+//             this.updateField(this.playerPosition, '*');
+//             this.playerPosition[1]--;
+//             this.updateField(this.playerPosition, '*');
+//         } else {
+//             console.log('You lose!!! move outside the field.')
+//             throw Error()
+//         }
+//     }
 
-    mvUp() {
-        if (this.playerPosition[1] - 1 >= 0) {
-            this.updateField(this.playerPosition, '*');
-            this.playerPosition[1]--;
-            this.updateField(this.playerPosition, '*');
-        } else {
-            console.log('You lose!!! You fell down a hole.')
-            throw Error()
-        }
-    }
+//     // field.length มีค่าเท่ากับ field[0].length(แกน x ซึ่งเท่ากับ 3)
+//     mvDown() {
+//         if (this.playerPosition[1] + 1 < this.field.length) {
+//             this.updateField(this.playerPosition, '*');
+//             this.playerPosition[1]++;
+//             this.updateField(this.playerPosition, '*');
+//         } else {
+//             console.log('You lose!!! move outside the field.')
+//             throw Error()
+//         }
+//     }
 
-    // board.length มีค่าเท่ากับ board[0].length(แกน x ซึ่งเท่ากับ 3)
-    mvDown() {
-        if (this.playerPosition[1] + 1 < this.board.length) {
-            this.updateField(this.playerPosition, '*');
-            this.playerPosition[1]++;
-            this.updateField(this.playerPosition, '*');
-        } else {
-            console.log('You lose!!! You fell down a hole.')
-            throw Error()
-        }
-    }
+//     updateField(position, Character) {
+//         const x = position[0];
+//         const y = position[1];
+//         switch (this.field[y][x]) {
+//             case '^':
+//                 throw Error('Congrat!! You found hat bro')
 
+//             case 'O':
+//                 throw Error('You lose!! falling in a hole.')
+//         }
+//         this.field[y][x] = Character
+//     }
 
-    updateField(position, Character) {
-        const x = position[0];
-        const y = position[1];
-        switch (this.board[y][x]) {
-            case '^':
-                console.log('Congrat!! You found hat bro')
-                break;
-        }
-        this.board[y][x] = Character
-
-
-    }
-
-    print() {
-        console.log(this.board.map((r) => r.join('')).join('\n'));
-    }
-}
-
-// const myField = new Field([
-//     ['*', '░', 'O'],
-//     ['░', 'O', '░'],
-//     ['░', '^', '░'],
-// ]);
-
-
-const player = new Field()
-
-while (true) {
-    const input = prompt('Which way? (w, a, s, d): ');
-    try {
-        if (input === 'w') {
-            player.mvUp();
-        } else if (input === 'a') {
-            player.mvLeft();
-        } else if (input === 's') {
-            player.mvDown();
-        } else if (input === 'd') {
-            player.mvRight();
-        } else {
-            console.log(`Invalid input ${input}`);
-        }
-    player.print();
-    } catch (error) {
-        console.log(error);
-        // console.clear();
-        break;
-        // console.log(this.playerPosition)
-        
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i=0; i<10; i++) {
-//     const x=Math.floor(Math.random()*4);
-//     console.log(x);
+//     print() {
+//         console.log(this.field.map((r) => r.join('')).join('\n'));
+//     }
 // }
+
+// // const myField = new Field([
+// //     ['*', '░', 'O'],
+// //     ['░', 'O', '░'],
+// //     ['░', '^', '░'],
+// //   ]);
+
+
+// const player = new Field()
+
+// while (true) {
+//     const input = prompt('Which way? (w, a, s, d): ');
+//     try {
+//         if (input === 'w') {
+//             player.mvUp();
+//         } else if (input === 'a') {
+//             player.mvLeft();
+//         } else if (input === 's') {
+//             player.mvDown();
+//         } else if (input === 'd') {
+//             player.mvRight();
+//         } else {
+//             console.log(`Invalid input please enter new value : ${input}`);
+//         }
+//         player.print();
+//     } catch (e) {
+//         console.log(e);
+//         break;
+//     }
+// }
+
+
+// // const blankarray = []
+// // const fieldRandom = ['░', 'O', '^']
+// // for (let i = 0; i < 5; i++) {
+// //     blankarray.push([])
+// //     for (let j = 0; j <5; j++) {
+// //         var item = fieldRandom[Math.floor(Math.random()*fieldRandom.length)];
+// //         blankarray[i].push(item)
+// //     }
+// // } console.log(blankarray[0][0] = '*') ;
+// // console.log(blankarray)
+
+// // console.log(blankarray.map((r) => r.join('')).join('\n'));
+
+
+// // // inputField = []
+// // // for (let i=0; i<1; i++) {
+// // //     const x=Math.floor((Math.random()*3) + 5);
+// // //     const y=Math.floor((Math.random()*3) + 5);
+// // //     inputField.push(x, y)
+// // //     // console.log(x);
+// // //     // console.log(y);
+// // // }
+// // // console.log(inputField)
+const height = 5
+const width = 5
+
+var field = []
+const fieldRandom = ['░']
+// const fieldRandom = ['░','O','^']
+
+for (let i = 0; i < height; i++) {
+    field.push([])
+    field[0][0] = '*'
+    for (let j = 0; j < width; j++) {
+        var item = fieldRandom[Math.floor(Math.random()*fieldRandom.length)];
+        field[i].push(item)
+    }
+} field[0][0] = '*';
+console.log(field);
